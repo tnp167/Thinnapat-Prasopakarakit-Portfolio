b704d8c71472ea1f3013864b42ecaf1bd51d7609
tnp167 <tnp_167@outlook.com>
2025-04-30 19:52:01 +0100

    Fix theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       <body className={roboto.className}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
